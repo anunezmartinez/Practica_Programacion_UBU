94606b332e3c58f5bf1513fdf9987c26aee7042a
Adrián Núñez Martínez <55793100+anunezmartinez@users.noreply.github.com>
2022-03-25 17:09:46 +0100

    Add files via upload

--- App/Main.c
+++ App/Main.c
@@ -7,42 +7,64 @@ int numeroTotalFilas(FILE *);
 int filaMasLarga(FILE *);
 
 
+
 int main(){
+    
+    FILE *fptr;
+    fptr = fopen("./Datos","r");
+    if(fptr == NULL){
+      printf("Error!");   
+      exit(1);             
+   }
 
-    //int variable opcion = solicitarOpcionMenu()
-
-    //selecionarOpcion(opcion);
+    int opcion = solicitarOpcionMenu();
+    selecionarOpcion(opcion);
 
     return 0;
 }
 
 int solicitarOpcionMenu(){
-//solicitar input int
-//validación del input
-//return input;
-}
-
-void selecionarOpcion(int){
-//while (!0)
-        //switch case (parametro int)
-            //case 1 - contarCaracteres();
-            //break
-            //case 2 - numeroTotalFilas();
-            //break
-            //case 3 - filaMasLarga();
-            //break
-            //0 - cambiamos el valor del while a 0 para romperlo
+    int myInt = 0;
+    printf("Introduce la opción a elegir : ");
+    scanf("%d", &myInt);
+    printf("%i", myInt);
+    if(myInt < 5 && myInt > -1){
+        return myInt;
+    } else{
+        printf("Error.");
+    }
 
 }
-int contarCaracteres(FILE *){
 
-//return caracterescontados;
+void selecionarOpcion(int seleccion){
+   
+    int bucle = 1;
+    while (bucle != 0){
+        switch(seleccion){
+            case 0:
+                bucle = 0;
+                break;
+            case 1:
+                contarCaracteres(fptr);
+                break;
+            case 2:
+                numeroTotalFilas(ftpr);
+                break;
+            case 3:
+                filaMasLarga(ftpr);
+                break;
+        }
+    }
 }
-int numeroTotalFilas(FILE *){
+int contarCaracteres(FILE *archivo){
 
-//return numeroFilas;
+return caracterescontados;
 }
-int filaMasLarga(FILE *){
+int numeroTotalFilas(FILE *archivo){
 
-//return filasLarga;
+return numeroFilas;
+}
+int filaMasLarga(FILE *archivo){
+
+return filasLarga;
 }
