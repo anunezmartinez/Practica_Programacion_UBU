d44bc4103838b2e0ba3eb36f9b7f4daabaaf323d
Adrián Núñez Martínez <55793100+anunezmartinez@users.noreply.github.com>
2022-03-25 16:45:30 +0100

    psudocódigo

--- App/Main.c
+++ App/Main.c
@@ -1,10 +1,48 @@
 #include <stdio.h>
 
+int solicitarOpcionMenu();
+void selecionarOpcion(int);
+int contarCaracteres(FILE *);
+int numeroTotalFilas(FILE *);
+int filaMasLarga(FILE *);
+
 
 int main(){
 
+    //int variable opcion = solicitarOpcionMenu()
 
-
+    //selecionarOpcion(opcion);
 
     return 0;
+}
+
+int solicitarOpcionMenu(){
+//solicitar input int
+//validación del input
+//return input;
+}
+
+void selecionarOpcion(int){
+//while (!0)
+        //switch case (parametro int)
+            //case 1 - contarCaracteres();
+            //break
+            //case 2 - numeroTotalFilas();
+            //break
+            //case 3 - filaMasLarga();
+            //break
+            //0 - cambiamos el valor del while a 0 para romperlo
+
+}
+int contarCaracteres(FILE *){
+
+//return caracterescontados;
+}
+int numeroTotalFilas(FILE *){
+
+//return numeroFilas;
+}
+int filaMasLarga(FILE *){
+
+//return filasLarga;
 }
